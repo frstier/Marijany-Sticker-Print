2682f290afc878340194119cba703a36a9bcc300
frstier <Frstier@gmail.com>
2025-12-22 14:12:39 +0200

    Updates

--- assets/index-r3ZSS53L.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C-HDlDQh.js","assets/index-mqFMUqhH.js"])))=>i.map(i=>d[i]);
-import{r as e,_ as t}from"./index-mqFMUqhH.js";const i=e("CapacitorZebraPrinter",{web:()=>t(()=>import("./web-C-HDlDQh.js"),__vite__mapDeps([0,1])).then(r=>new r.CapacitorZebraPrinterWeb)});export{i as CapacitorZebraPrinter};

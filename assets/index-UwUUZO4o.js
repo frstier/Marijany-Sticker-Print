6d1fce62acdf292d20944c15371c79c552316e3f
frstier <Frstier@gmail.com>
2026-01-05 10:51:09 +0200

    Updates

--- assets/index-UwUUZO4o.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CNhUnEoD.js","./index-DQhoi_pc.js","./index-BRGWD6rd.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as t}from"./index-DQhoi_pc.js";const i=e("CapacitorZebraPrinter",{web:()=>t(()=>import("./web-CNhUnEoD.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.CapacitorZebraPrinterWeb)});export{i as CapacitorZebraPrinter};
---
+++ assets/index-WOIRnm5K.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BKHAqI2x.js","./index-nJ0aCjQ1.js","./index-BRGWD6rd.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as t}from"./index-nJ0aCjQ1.js";const i=e("CapacitorZebraPrinter",{web:()=>t(()=>import("./web-BKHAqI2x.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.CapacitorZebraPrinterWeb)});export{i as CapacitorZebraPrinter};

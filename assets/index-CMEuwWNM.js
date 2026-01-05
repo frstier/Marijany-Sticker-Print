38e65cb074b0aa273cea9c592c80b1bacd826a33
frstier <Frstier@gmail.com>
2026-01-05 09:58:15 +0200

    Updates

---
+++ assets/index-CMEuwWNM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DdiTpOxX.js","assets/index-BvY8fA51.js","assets/index-BRGWD6rd.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as t}from"./index-BvY8fA51.js";const i=e("CapacitorZebraPrinter",{web:()=>t(()=>import("./web-DdiTpOxX.js"),__vite__mapDeps([0,1,2])).then(r=>new r.CapacitorZebraPrinterWeb)});export{i as CapacitorZebraPrinter};
